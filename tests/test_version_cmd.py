459e5428b161076db636732c08eecf52dbe428e2
tdejoigny-ledger <thibault.de-joigny@ledger.fr>
2025-09-26 20:15:50 +0200

    add apex support & remove nanoS support

--- tests/test_version_cmd.py
+++ tests/test_version_cmd.py
@@ -3,7 +3,7 @@ from application_client.kaspa_response_unpacker import unpack_get_version_respon
 
 # Taken from the Makefile, to update every time the Makefile version is bumped
 MAJOR = 1
-MINOR = 1
+MINOR = 2
 PATCH = 0
 
 # In this test we check the behavior of the device when asked to provide the app version
